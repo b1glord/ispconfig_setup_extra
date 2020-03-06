d95eba21943236455b0114bb857bd1bd3eecce88
b1glord <FuryTr@yandex.com>
2020-03-06 16:18:13 +0300

    clear

--- centos8/README.md
+++ centos8/README.md
@@ -1,7 +1,4 @@
 ## ispconfig_setup_extra
 
-https://github.com/b1glord/ispconfig_setup_extra/wiki
 
-## restart ispconfig
-/usr/local/ispconfig/server/server.sh
 
---
+++ centos8/epel/install_epel.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------
+# install_epel.sh
+#
+# ISPConfig 3 centos7 hhvm facebook installer
+#
+# Script: install_epel.sh
+# Version: 1.0.0
+# Author: BigLorD <furytr@yandex.com>
+# Description: This script will install all the packages needed to install
+# ISPConfig 3 on your server.
+#
+#
+#---------------------------------------------------------------------
+#Ref: https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F
+ 
+ echo -n "Enable the Centos 7 EPEL repository "
+	dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
