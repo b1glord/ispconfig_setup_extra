6d4d96c0491d7d8022ff0b7699723b8407d41a87
BigLorD <FuryTr@yandex.com>
2020-03-11 23:57:45 +0300

    Update install_hhvm_linuxeye.sh

--- centos7/hhvm/install_hhvm_linuxeye.sh
+++ centos7/hhvm/install_hhvm_linuxeye.sh
@@ -42,7 +42,8 @@ sed -i "s%date.timezone = Asia/Calcutta%date.timezone = Europe/Istanbul%" /etc/h
  echo "[Service]" >> /etc/systemd/system/hhvm.service
  echo "ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.ini --user apache2 --mode daemon -vServer.Type=fastcgi -  vServer.FileSocket=/var/run/hhvm/hhvm.sock" >> /etc/systemd/system/hhvm.service
 #echo "ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.ini --user nginx --mode daemon -vServer.Type=fastcgi -  vServer.FileSocket=/var/run/hhvm/hhvm.sock" >> /etc/systemd/system/hhvm.service
- echo "" >> /etc/systemd/system/hhvm.service
+#echo "ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.hdf --user nobody --mode daemon -vServer.Type=fastcgi -vServer.Port=9009" >> /etc/systemd/system/hhvm.service
+echo "" >> /etc/systemd/system/hhvm.service
  echo "[Install]" >> /etc/systemd/system/hhvm.service
  echo "WantedBy=multi-user.target" >> /etc/systemd/system/hhvm.service
 
