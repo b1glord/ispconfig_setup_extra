d6c842ec81af4b784c9571dc57cab75a122316b4
BigLorD <FuryTr@yandex.com>
2020-03-17 18:01:02 +0300

    Update hhvminfo.php

--- centos7/hhvm/hhvminfo.php
+++ centos7/hhvm/hhvminfo.php
@@ -1,16 +1,4 @@
 <?php
-function is_hhvm() {
-    return defined('HHVM_VERSION');
-}
-
-if (is_hhvm()) {
-    echo HHVM is working\n;
-} else {
-    echo HHVM is not working\n;
-}
-?>
-
-<?php
 if (defined('HHVM_VERSION')) {
  echo 'Hip Hop Virtual Machine is working';
 }
