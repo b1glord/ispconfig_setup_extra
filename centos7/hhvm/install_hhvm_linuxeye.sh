01d3e56c9e392c5e200c44da07d7a78175d7f8d8
BigLorD <FuryTr@yandex.com>
2020-03-18 19:05:52 +0300

    Update install_hhvm_linuxeye.sh

--- centos7/hhvm/install_hhvm_linuxeye.sh
+++ centos7/hhvm/install_hhvm_linuxeye.sh
@@ -25,36 +25,24 @@
 #https://www.howtoforge.com/tutorial/how-to-install-wordpress-with-hhvm-and-nginx-on-centos-7/#step-configure-hhvm-and-nginx
 #http://mirrors.linuxeye.com/hhvm-repo/7/x86_64/
 
- echo -n "Installing HHVM HipHop Virtual Machine (FCGI)... "
+  yum -y install cpp gcc-c++ cmake git psmisc {binutils,boost,jemalloc,numactl}-devel \
+{ImageMagick,sqlite,tbb,bzip2,openldap,readline,elfutils-libelf,gmp,lz4,pcre}-devel \
+lib{xslt,event,yaml,vpx,png,zip,icu,mcrypt,memcached,cap,dwarf}-devel \
+{unixODBC,expat,mariadb}-devel lib{edit,curl,xml2,xslt}-devel \
+glog-devel oniguruma-devel ocaml gperf enca libjpeg-turbo-devel openssl-devel \
+mariadb mariadb-server make libc-client 
 
-touch /etc/yum.repos.d/hhvm.repo
-echo "[hhvm]" >> /etc/yum.repos.d/hhvm.repo
-echo "name=gleez hhvm-repo" >> /etc/yum.repos.d/hhvm.repo
-echo "baseurl=http://mirrors.linuxeye.com/hhvm-repo/7/\$basearch/" >> /etc/yum.repos.d/hhvm.repo
-echo "enabled=1" >> /etc/yum.repos.d/hhvm.repo
-echo "gpgcheck=0" >> /etc/yum.repos.d/hhvm.repo
-
- yum -y install zeromq-devel hhvm
+  echo -n "Installing HHVM HipHop Virtual Machine (FCGI)... "
+   yum -y install zeromq-devel
+rpm -Uvh http://mirrors.linuxeye.com/hhvm-repo/7/x86_64/hhvm-3.15.3-1.el7.centos.x86_64.rpm
 
 # Configure Hhvm (optional)
- ln -s /usr/local/bin/hhvm /bin/hhvm
  mkdir /var/run/hhvm
  mkdir /var/log/hhvm
-# Change the admin port
-sed -i "s%date.timezone = Asia/Calcutta%date.timezone = $TIME_ZONE%" /etc/hhvm/server.ini
-
- echo "[Unit]" >> /etc/systemd/system/hhvm.service
- echo "Description=HHVM HipHop Virtual Machine (FCGI)" >> /etc/systemd/system/hhvm.service
- echo "After=network.target nginx.service mariadb.service" >> /etc/systemd/system/hhvm.service
- echo "" >> /etc/systemd/system/hhvm.service
- echo "[Service]" >> /etc/systemd/system/hhvm.service
-
-#echo "ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.ini --user nginx --mode daemon hhvm.server.type=fastcgi hhvm.server.file_socket=/var/run/hhvm/hhvm.sock" >> /etc/systemd/system/hhvm.service
-#echo "ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.ini --user nginx --mode daemon hhvm.server.type=fastcgi hhvm.server.port=9001" >> /etc/systemd/system/hhvm.service
-
-echo "" >> /etc/systemd/system/hhvm.service
- echo "[Install]" >> /etc/systemd/system/hhvm.service
- echo "WantedBy=multi-user.target" >> /etc/systemd/system/hhvm.service
+ TIME_ZONE=$(echo "$TIME_ZONE" | sed -n 's/ (.*)$//p')
+ sed -i "s%date.timezone = Asia/Calcutta%date.timezone = $TIME_ZONE%" /etc/hhvm/server.ini
+# Configure hhvm.service (optional)
+sed -i "s%ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.ini --user nginx --mode daemon -vServer.Type=fastcgi -vServer.Port=9001%ExecStart=/usr/local/bin/hhvm --config /etc/hhvm/server.ini --user nginx --mode daemon -vServer.Type=fastcgi -vServer.Port=9001 -vLog.Level=Debug -vLog.File=/var/log/hhvm/hhvm.log%" /etc/hhvm/server.ini
 
 systemctl daemon-reload
 systemctl restart hhvm.service
