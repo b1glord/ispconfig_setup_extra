fa6994ab4b4c42caf039e52bdccad3c7cfca7726
BigLorD <FuryTr@yandex.com>
2020-03-22 18:15:46 +0300

    Update install_github.sh

--- centos7/git/install_github.sh
+++ centos7/git/install_github.sh
@@ -14,8 +14,8 @@
 #---------------------------------------------------------------------
  #ref https://www.howtodojo.com/2017/10/install-git-centos-7/
   echo -n "Removing Git (Old Version)... "
-  yum remove -y git
+  yum -y remove git
   echo -n "Installing Git (New Version)... "
-  yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-  yum install -y git2u
+  yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+  yum -y install git2u
   git --version
