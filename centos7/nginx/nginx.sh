bfa74c97d626a74ffa52ec1f7732bf078df0eeee
BigLorD <FuryTr@yandex.com>
2020-03-22 18:20:38 +0300

    Update nginx.sh

--- centos7/nginx/nginx.sh
+++ centos7/nginx/nginx.sh
@@ -7,7 +7,7 @@ yum install -y nginx
 yum install -y nginx-all-modules nginx-mod-http-geoip nginx-mod-http-image-filter nginx-mod-http-perl nginx-mod-http-xslt-filter nginx-mod-mail nginx-mod-stream
 
 # Configure Nginx Default Port 81
-sed -i "s/    listen       80;/    listen       81;/" /etc/nginx/conf.d/default.conf
+#sed -i "s/    listen       80;/    listen       81;/" /etc/nginx/conf.d/default.conf
 
 sed -i "s/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php.ini
 
