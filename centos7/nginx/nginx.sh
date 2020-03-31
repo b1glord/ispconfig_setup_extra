da6bda4d10159fee698e648d6390cbbc97481d78
BigLorD <FuryTr@yandex.com>
2020-03-31 12:46:23 +0300

    Update nginx.sh

--- centos7/nginx/nginx.sh
+++ centos7/nginx/nginx.sh
@@ -1,6 +1,14 @@
 #https://nginx.org/en/linux_packages.html
 # Add nginx Repo
-wget -nc https://raw.githubusercontent.com/b1glord/ispconfig_setup_extra/master/nginx/nginx.repo -P /etc/yum.repos.d
+  cat > /etc/yum.repos.d/nginx.repo <<EOF
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+EOF
+
 
 yum update
 yum install -y nginx
