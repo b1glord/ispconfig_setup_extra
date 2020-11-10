b5ec6432ca6bb1df251f05a790722ca7fd81f7b3
BigLorD <FuryTr@yandex.com>
2020-11-10 12:20:28 +0300

    Update mssql-2017.sh

--- centos7/mssql/mssql-2017.sh
+++ centos7/mssql/mssql-2017.sh
@@ -1,4 +1,12 @@
 # https://packages.microsoft.com/config/rhel/7/
 wget https://packages.microsoft.com/config/rhel/7/mssql-server-2017.repo -O /etc/yum.repos.d/mssql-server-2017.repo
+yum -y install expect
+yum -y install mssql-server
+sudo /opt/mssql/bin/mssql-conf setup
+#Enter your edition(1-8):
+3
+#Choose the language for SQL Server:
+1
+#Enter the SQL Server system administrator password:
+Pass1234
 
-yum install mssql-server -y
