641e692d6051db12569895b35381dd311882b3b6
BigLorD <FuryTr@yandex.com>
2020-11-23 17:33:29 +0300

    Update and rename readme.md to pdns.sh

--- centos7/powerdns/pdns.sh
+++ centos7/powerdns/pdns.sh
@@ -1,6 +1,7 @@
+yum -y install powerdns
 
-'''
+```
 create database powerdns;
 create user 'powerdns'@'localhost' IDENTIFIED BY 'MYSQL_PAROLASI';
 grant all privileges on powerdns.* to powerdns@localhost;
-'''
+```
